ba0e9517856cddb7678ca9c8f1ec4b1f24a4c344
Piyush Singh <106422266+piyush18singh@users.noreply.github.com>
2026-01-10 14:33:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,54 @@
-//complete this code
-class Rectangle {}
+class Rectangle {
+  constructor(width, height) {
+    if (width <= 0 || height <= 0) {
+      throw new Error("Width and height must be positive numbers");
+    }
+    this._width = width;
+    this._height = height;
+  }
 
-class Square extends Animal {}
+  // getter for width
+  get width() {
+    return this._width;
+  }
 
-// Do not change the code below this line
+  // getter for height
+  get height() {
+    return this._height;
+  }
+
+  // method to calculate area
+  getArea() {
+    return this._width * this._height;
+  }
+}
+
+class Square extends Rectangle {
+  constructor(side) {
+    if (side <= 0) {
+      throw new Error("Side must be a positive number");
+    }
+    super(side, side);
+  }
+
+  // method to calculate perimeter
+  getPerimeter() {
+    return 4 * this.width;
+  }
+}
+
+/* Make classes global for Cypress tests */
 window.Rectangle = Rectangle;
 window.Square = Square;
+
+/* Example test in console */
+const rectangle = new Rectangle(5, 10);
+console.log(rectangle.width);      // 5
+console.log(rectangle.height);     // 10
+console.log(rectangle.getArea());  // 50
+
+const square = new Square(7);
+console.log(square.width);         // 7
+console.log(square.height);        // 7
+console.log(square.getArea());     // 49
+console.log(square.getPerimeter()); // 28
